8e72f48ec56842cb2b76f1920a1f5f376a24317f
Nguyen Duc Thinh <thinhdz1500@gmail.com>
2025-04-01 17:20:02 +0700

    bai2 phan 3

--- docker-compose/phan2/node-app/app.js
+++ docker-compose/phan2/node-app/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb://mongo:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js!');
+  res.send('Hello from Node.js with MongoDB!');
 });
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log('Server running on port 3000');
 });
